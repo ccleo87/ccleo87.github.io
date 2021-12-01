f30c8866b2dc75609775fd594d10dfcca569976e
Cleo Chow <cleo.chow@berkeley.edu>
2021-12-01 00:00:57 -0800

    wdd final project

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function toggleTheme() {
+    let body = document.querySelector('body');
+    body.classList.toggle('dark-mode');
+
+    let button = document.querySelector('[id^="toggleButton"]');
+    let buttonActive = button.id.includes("inactive");
+
+    // active = lightmode -> darkmode
+    // inactive = darkmode -> lightmode
+    if (!buttonActive) {
+        button.id = "toggleButton inactive";
+        button.src = "assets/img/lightmode.svg";
+    } else {
+        button.id = "toggleButton active";
+        button.src = "assets/img/darkmode.svg";
+    }
+}
+
+
+let toggleBttnVariable = document.querySelector('[id^="toggleButton"]');
+toggleBttnVariable.onclick = toggleTheme;
+
+// $(window).scroll(function() {
+//     if ($(this).scrollTop()) {
+//         $('#directionBlock:hidden').stop(true, true).fadeIn();
+//     } else {
+//         $('#directionBlock').stop(true, true).fadeOut();
+//     }
+// });
